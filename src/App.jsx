ab7c6e5eca3e1d256de1a04a999f03d8089a8e2c
Joann Carter <jocarter@Joanns-MacBook-Pro.local>
2024-12-07 13:02:08 -0500

    add hero sectuion

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import "./App.css";
-
+import Hero from "./components/Hero";
 function App() {
   return (
     <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Hero />
     </>
   );
 }
