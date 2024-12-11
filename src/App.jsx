64087e91bb9c6a0b134289532ae5b386fa14f0a6
Joann Carter <jocarter@Joanns-MacBook-Pro.local>
2024-12-10 21:37:53 -0500

    add contact footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,21 @@ import Hero from "./components/Hero";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
 import About from "./components/About";
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
-      <div className=""></div>
       <div className="App">
-        <div className="scroll-section">
-          <Hero />
-        </div>
-        <div className="scroll-section">
-          <Header content={"Projects"} />
-          <Projects />
-        </div>
-        <div className="scroll-section">
-          <Header content={"About"} />
-          <About />
-        </div>
+        <Hero />
+
+        <Header content={"Projects"} />
+        <Projects />
+
+        <Header content={"About"} />
+        <About />
+
+        <Header content={"Contact"} />
+        <Footer />
       </div>
     </>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import "./styles/footer.css";
+import Header from "./Header";
+
+export default function Footer() {
+  return (
+    <div className="footer-container">
+      {/* <h1 className="footer-title">Contact Me</h1> */}
+
+      <p className="footer-email"> Joann333Carter@gmail.com</p>
+      <p className="footer-phone"> (603)566-5610</p>
+    </div>
+  );
+}
